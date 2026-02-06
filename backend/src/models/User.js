793316abd16b977bc009bf4ce26be3d7fed6cae6
Sudeep <sudeeppprajapati@gmail.com>
2026-02-06 14:35:27 +0530

    add model Schema

---
+++ backend/src/models/User.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+});
+
+userSchema.pre("save", async function () {
+    if (!this.isModified("password")) return;
+    this.password = await bcrypt.hash(this.password, 10);
+});
+
+userSchema.methods.matchPassword = function (enteredPassword) {
+    return bcrypt.compare(enteredPassword, this.password);
+};
+
+export default mongoose.model("User", userSchema);
